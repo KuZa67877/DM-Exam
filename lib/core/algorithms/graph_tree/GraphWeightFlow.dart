750f9b6ea4e608ba184ac7822dc5177337ce55c3
ZhanRober <borisov.bip@gmail.com>
2024-05-25 19:06:23 +0300

    GraphWeight

---
+++ lib/core/algorithms/graph_tree/GraphWeightFlow.dart
@@ -0,0 +1,149 @@
+import 'GraphWeight.dart';
+import 'dart:math';
+import 'dart:collection';
+
+class GraphWeightFlow extends GraphWeight
+{
+  //я не придумал как релаизовать адекватней. Здесь списки вершин и куда из них можно простроить путь, окромя базового контура
+  Map<int, Set<int>> neighbours = {
+    2 : {3,6,7},
+    3 : {2,4,5,6,7,8,10},
+    4 : {3,5,6},
+    5 : {6,9,10},
+    6 : {5,8,9,10,7},
+    7 : {8,9},
+    8 : {9},
+    9 : {8, 10},
+    10: {9}
+  };
+  //Мапа для подсчёта количества рёбер, приходящих в граф, чтобы граф не был перенасыщен
+  Map<int, int> vertex_to = {
+    2:1,
+    3:0,
+    4:1,
+    5:1,
+    6:0,
+    7:1,
+    8:1,
+    9:0,
+    10:1
+  };
+  //Мапа загруженности вершины (количество входящих и выходящих рёбер)
+  Map<int, int> vertex_summ = {
+    2:2,
+    3:1,
+    4:2,
+    5:2,
+    6:0,
+    7:2,
+    8:2,
+    9:1,
+    10:2
+  };
+
+  @override
+  void generate_graph() 
+  {
+    create_empty_graph(11);
+    var start = 1;
+    var finish = 11;
+    var random = Random();
+
+    //заполняем базовыми рёбрами, которые необходимы для адекватного веса
+
+    for(var i = 2; i<=4; i++)
+    {
+      // w_graph[start]![i] = random.nextInt(15)+4;
+      w_graph[start]?[i] = w_graph[start]?[i] ?? random.nextInt(15)+4;
+
+    }
+    for(var i = 10; i>=8; i--)
+    {
+      // w_graph[i]![finish] = random.nextInt(12)+7;
+      w_graph[i]?[finish] = w_graph[i]?[finish] ?? random.nextInt(12)+7;
+    }
+    for(var i in {2,4,5,7})
+    {
+      // w_graph[i]![i+3] = random.nextInt(15)+4;
+      w_graph[i]?[i+3] = w_graph[i]?[i+3] ?? random.nextInt(15)+4;
+    }
+    //а теперь заполняем всё остальное, связывая вершины на рандом возможными рёбрами
+    for(var paths in neighbours.entries) 
+    {
+      for(var vertex in paths.value)
+      {
+        //проверяем, чтобы из вершины, куда строим ребро, не шло ребро в текущую вершину
+        bool fl = w_graph[vertex]?.containsKey(paths.key) ?? false;
+        // проверяем, чтобы вершины не были перегружены
+        bool fl_len = (vertex_summ[vertex] ?? 0) < 3 && (vertex_summ[paths.key] ?? 0) < 3;
+
+        if(!fl && fl_len && random.nextDouble() < 0.3)
+        {
+          w_graph[paths.key]![vertex] = random.nextInt(15)+4;
+          //заполняем данные о загруженности впершин
+          vertex_to[vertex] = (vertex_to[vertex] ?? 0) + 1;
+          vertex_summ[vertex] = (vertex_summ[vertex] ?? 0) + 1;
+          vertex_summ[paths.key] = (vertex_summ[paths.key] ?? 0) + 1;
+        }
+      }
+    } 
+    check_fill();
+  }
+  void check_fill()
+  {
+    var random = Random();
+    for(var pair in vertex_to.entries)
+    {
+      if(pair.value==0) //если в эту вершину нет путей
+      {
+        var sortedEntries = vertex_summ.entries.toList()
+        ..sort((a, b) => a.value.compareTo(b.value));
+
+        // Создаем новую LinkedHashMap из отсортированных пар
+        LinkedHashMap<int, int> sortedMap = LinkedHashMap.fromEntries(sortedEntries);
+        print(sortedMap);
+        for(var vertexes in sortedMap.entries)
+        {
+          //проверяем, нет ли уже обратного пути
+          bool fl = w_graph[pair.key]?.containsKey(vertexes.key) ?? false;
+          //проверка, можно ли вообще в эту вершину путь построить
+          if((neighbours[vertexes.key]?.contains(pair.key) ?? false) && !fl)
+          {
+            print(vertexes.key);
+            print(pair.key);
+            w_graph[vertexes.key]![pair.key] = random.nextInt(15)+4;
+
+            vertex_to[pair.key] = (vertex_to[pair.key] ?? 0) + 1;
+            vertex_summ[vertexes.key] = (vertex_summ[vertexes.key] ?? 0) + 1;
+            vertex_summ[pair.key] = (vertex_summ[pair.key] ?? 0) + 1;
+            break;
+          }
+        }
+      }
+      if(pair.value==vertex_summ[pair.key]) //если в эту вершину есть пути, но из неё нет
+      {
+        var sortedEntries = vertex_summ.entries.toList()
+        ..sort((a, b) => a.value.compareTo(b.value));
+
+        // Создаем новую LinkedHashMap из отсортированных пар
+        LinkedHashMap<int, int> sortedMap = LinkedHashMap.fromEntries(sortedEntries);
+        print(sortedMap);
+        for(var vertexes in sortedMap.entries)
+        {
+          //проверяем, нет ли уже обратного пути
+          bool fl = w_graph[pair.key]?.containsKey(vertexes.key) ?? false;
+          //проверка, можно ли вообще в эту вершину путь построить
+          if((neighbours[pair.key]?.contains(vertexes.key) ?? false) && fl)
+          {
+            w_graph[pair.key]![vertexes.key] = random.nextInt(15)+4;
+
+            vertex_to[vertexes.key] = (vertex_to[vertexes.key] ?? 0) + 1;
+            vertex_summ[vertexes.key] = (vertex_summ[vertexes.key] ?? 0) + 1;
+            vertex_summ[pair.key] = (vertex_summ[pair.key] ?? 0) + 1;
+            break;
+          }
+        }
+      }
+    }
+  }
+}
---
+++ lib/core/algorithms/graph_tree/GraphWeightPath.dart
@@ -0,0 +1,95 @@
+import 'GraphWeight.dart';
+import 'dart:math';
+import 'dart:collection';
+
+class GraphWeightPath extends GraphWeight
+{
+  //список с количеством выходов из вершин
+  Map<int, int> vertexes_to= {};
+  //список с количеством входом в вершины
+  Map<int, int> vertexes_in= {};
+  @override
+  void generate_graph() 
+  {
+    var random = Random();
+    create_empty_graph(5);
+
+    for (var i = 1; i <= w_graph.length; i++)
+    {
+      vertexes_to[i] = 0;
+      vertexes_in[i] = 0;
+    }
+
+    var fl = true;
+
+    MapEntry<int, int> min_to = find_min(vertexes_to); //вершина из которой пойдём
+    MapEntry<int, int> min_in = find_min(vertexes_in); //вершина в которую пойдём
+
+    while((min_to.value < 3 || min_in.value<3) && fl)
+    {
+      bool was_reverse = (w_graph[min_in.key]?.containsKey(min_to.key) ?? false); //есть уже обратный путь
+      bool was_yet = (w_graph[min_to.key]?.containsKey(min_in.key) ?? false); //уже существует путь, который мы хотим построить
+
+      if(min_to.key!=min_in.key && !(was_yet||was_reverse)) //если вершины не совпадают и два условия выше выполняются
+      {
+        w_graph[min_to.key]?[min_in.key] = random.nextInt(5)+3;
+        vertexes_to[min_to.key] = (vertexes_to[min_to.key] ?? 0) + 1;
+        vertexes_in[min_in.key] = (vertexes_in[min_in.key] ?? 0) + 1;
+      }
+      else
+      {
+        //флаг мб можно будет убрать, он необходим был для старой версии алгоритма
+        fl = false;
+
+        var sortedEntries_in = vertexes_in.entries.toList()
+        ..sort((a, b) => a.value.compareTo(b.value));
+        LinkedHashMap<int, int> sorted_in = LinkedHashMap.fromEntries(sortedEntries_in);
+        //отсортировали мапу и идём по ней, пока не будет вершины, в которую можно проложить путь
+        for(var ver_in in sorted_in.entries)
+        {
+          //те же условия на наличия путей
+          was_reverse = (w_graph[ver_in.key]?.containsKey(min_to.key) ?? false);
+          was_yet = (w_graph[min_to.key]?.containsKey(ver_in.key) ?? false);
+          //если сопали или есть пути, продолжаем перебор
+          if(min_to.key == ver_in.key || (was_yet||was_reverse)) continue;
+          else
+          {
+            w_graph[min_to.key]?[ver_in.key] = random.nextInt(5)+3;
+            vertexes_to[min_to.key] = (vertexes_to[min_to.key] ?? 0) + 1;
+            vertexes_in[ver_in.key] = (vertexes_in[ver_in.key] ?? 0) + 1;
+            fl = true; //мы не перебрали все варианты -> нашли подходящий -> можно продолжать цикл
+            break;
+          }
+        }
+      }
+      min_to = find_min(vertexes_to);
+      min_in = find_min(vertexes_in);
+    }
+  }
+  //рандомно выбираем вершину, так, чтобы количество входов или выходов в неё было минимальным (выбираем вершину среди минимальных)
+  MapEntry<int, int> find_min(Map<int,int> count_of_paths)
+  {
+    if(count_of_paths.isEmpty) return (MapEntry(1,0));
+    List<int> fit_ver = [];
+    int min_count = 1024;
+    for(var pair in count_of_paths.entries)
+    {
+      if(pair.value<min_count)
+      {
+        fit_ver.clear();
+        fit_ver.add(pair.key);
+        min_count = pair.value;
+      }
+      else if(pair.value == min_count)
+      {
+        fit_ver.add(pair.key);
+      }
+    }
+    var random = Random();
+    var len = fit_ver.length-1;
+    var ind = 0;
+    if(len>0) ind = random.nextInt(len);
+    var element = fit_ver[ind];
+    return MapEntry(element, count_of_paths[element]??0);
+  }
+}
