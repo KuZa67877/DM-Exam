7265b7d6bfcbbfff760cb1aa680b411f476838df
ZhanRober <borisov.bip@gmail.com>
2024-06-02 01:04:19 +0300

    Обнова

--- lib/core/algorithms/graph_tree/Node.dart
+++ lib/core/algorithms/graph_tree/Node.dart
@@ -2,20 +2,21 @@ class Node
 {
   var _name;
   List<Node?> _childs = [];
+  var _height;
 
-  Node([this._name = 0]);
+  Node(this._name);
 
   int get name {return this._name;} 
   List<Node?> get childs {return this._childs;} 
+
+  set childs(List<Node?> list) => this._childs = list;
+
   set name(int val) => this._name = val; 
 
-  set_child(Node? element, int ind)
+  int get height
   {
-    _childs[ind] = element ?? null;
+    int leftHeight = _childs[0]?.height ?? 0;
+    int rightHeight = _childs[1]?.height ?? 0;
+    return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
   }
-
-  // void print_node()
-  // {
-  //   print('$_name : $adj_ver \n');
-  // }
 }
